7f33321ff4b2847014485adeba3c4d98b627d362
Ajinkya Abhyankar <aabhyankar9@gmail.com>
2016-04-12 20:03:34 +0530

    Added files via upload

--- crime_data_analysis.py
+++ crime_data_analysis.py
@@ -9,6 +9,7 @@ def read_csvdataset():
     df=pd.DataFrame(crime_data)
     return df
 
+#function to visualize count by crime category
 def count_by_crime_category(df):
     df['Crime_Category'].value_counts().sort_values(ascending=True).plot(kind='barh')
     plt.title("Data Representation Of Crime By Category")
@@ -16,5 +17,21 @@ def count_by_crime_category(df):
     plt.xlabel('Number of Crimes per category')
     plt.show()
 
+#function to visualize count by crime location
+def count_by_crime_location(df):
+    #top 20 locations with higher crimes
+    fracs=[x for x in df['Location'].value_counts().sort_values(ascending=False).head(20)]
+    df['Location'].value_counts().sort_values(ascending=False).head(20).plot(kind='barh').plot()
+    plt.title("Data Representation Of Crime By Location : Areas more prone to crimes")
+    plt.show()
+
+    # bottom 20 location with less crimes
+    df['Location'].value_counts().sort_values(ascending=False).tail(20).plot(kind='pie')
+    plt.title("Data Representation Of Crime By Location : Areas less prone to crime")
+    plt.show()
+
+
 df=read_csvdataset()
+print(df['Location'].value_counts().min(),df['Location'].value_counts().sort_values(ascending=True))
 count_by_crime_category(df)
+count_by_crime_location(df)
